e03f5d6d29783f30998351d948c61da38fcc5cea
abc <you@example.com>
2019-10-18 18:14:29 +0100

    Key Update

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 const config = {
 
   WEATHER_API: '33979dcdd962c94eb6a528b5cd7bf714',
-  GOOGLE_API: 'AIzaSyCILKU9LwO6qKSWwTUCEhayOuYMeIXj4i0'
-
+  GOOGLE_API: 'AIzaSyCynrG2zeVLTtJiZQxV37R8c86-k8S3V4s'
 }
